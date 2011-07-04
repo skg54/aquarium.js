75d1656614c11907c24209aa6f90c53fb073fa23
Paulo Da Silva <paulothesilva@gmail.com>
2011-07-04 16:44:30 -0400

    First version Fish can swim but it needs a better algorithm Need to implement better collision detection, atm it doesnt take into account aquarium offset or fish size

---
+++ main.js
@@ -0,0 +1,92 @@
+var ANIMATION_INTERVAL = 100;
+
+var FISH_MAX_VELOCITY = 6;
+var FISH_SPEED_INCREMENT = 4;
+
+var AQUARIUM_TOTAL_FISH = 4;
+var AQUARIUM_LEFT_EDGE = 0;
+var AQUARIUM_RIGHT_EDGE = 640;
+var AQUARIUM_TOP_EDGE = 0;
+var AQUARIUM_BOTTOM_EDGE = 480;
+
+var fish = {};
+
+function randomNextInt() {
+    return Math.floor(Math.random() * 101);
+}
+
+function Fish() {
+    this.x = 0;
+    this.y = 0;
+    this.velocityX = 0;
+    this.velocityY = 0;
+}
+
+Fish.prototype.move = function() {
+    if (randomNextInt() % 9 < 1) {
+        this.velocityX += randomNextInt() % FISH_SPEED_INCREMENT;
+        this.velocityX = Math.min(this.velocityX, FISH_MAX_VELOCITY);
+        this.velocityX = Math.max(this.velocityX, -FISH_MAX_VELOCITY);
+
+        this.velocityY += randomNextInt() % FISH_SPEED_INCREMENT;
+        this.velocityY = Math.min(this.velocityY, FISH_MAX_VELOCITY);
+        this.velocityY = Math.max(this.velocityY, -FISH_MAX_VELOCITY);
+    }		
+
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+
+    if (this.x < AQUARIUM_LEFT_EDGE) {
+        this.x = AQUARIUM_LEFT_EDGE;
+        this.velocityX = -this.velocityX;
+    }
+    else if (this.x > AQUARIUM_RIGHT_EDGE) {
+        this.x = AQUARIUM_RIGHT_EDGE;
+        this.velocityX = -this.velocityX;
+    }
+
+    if (this.y < AQUARIUM_TOP_EDGE) {
+        this.y = AQUARIUM_TOP_EDGE;
+        this.velocityY = -this.velocityY;
+    }
+    else if (this.y > AQUARIUM_BOTTOM_EDGE) {
+        this.y = AQUARIUM_BOTTOM_EDGE ;
+        this.velocityY = -this.velocityY;
+    }
+}
+
+function createFish() {
+    var aquarium = $('#aquarium');
+    AQUARIUM_LEFT_EDGE = aquarium.offset.left;
+    AQUARIUM_TOP_EDGE = aquarium.offset.top;
+
+    for(var i = 0; i < AQUARIUM_TOTAL_FISH; i++) {
+        var newFish = new Fish();
+        newFish.x = AQUARIUM_RIGHT_EDGE / 2;
+        newFish.y = AQUARIUM_BOTTOM_EDGE / 2; 
+
+        var fishID = "fish" + i;
+        fish[fishID] = newFish;
+
+        var fishElement = $('<li>',  {class: 'fish',  id: fishID});
+        aquarium.append(fishElement);    
+    }
+}
+
+function animate() {
+    $('.fish').each(function() {
+        var fishID = $(this).attr('id');
+        var f = fish[fishID];
+        f.move();
+        $(this).offset({ left: f.x, top: f.y });
+    });
+}
+
+function setupAquarium() {
+    createFish();
+}
+
+$(function() {
+    setupAquarium();
+    setInterval(animate, ANIMATION_INTERVAL);
+});
